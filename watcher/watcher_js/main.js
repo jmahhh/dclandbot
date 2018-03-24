d2866f3b9a9a474d9ac86f97eea20c6ebf604a47
jmahhh <hubertjoel@gmail.com>
2018-03-24 10:09:16 +0800

    reverse x and y coords in decodeTokenId

--- watcher/watcher_js/main.js
+++ watcher/watcher_js/main.js
@@ -20,8 +20,8 @@ const base = new BigNumber('0x100000000000000000000000000000000');
 
 const decodeTokenId = (hexRepresentation) => {
   const yLength = hexRepresentation.length - 32;
-  let x = new BigNumber('0x' + hexRepresentation.slice(yLength, hexRepresentation.length));
-  let y = new BigNumber('0x' + (hexRepresentation.slice(0, yLength) || '0'));
+	let x = new BigNumber('0x' + (hexRepresentation.slice(0, yLength) || '0'));
+  let y = new BigNumber('0x' + hexRepresentation.slice(yLength, hexRepresentation.length));
   if (x.gt(max)) x = base.minus(x).times(-1);
   if (y.gt(max)) y = base.minus(y).times(-1);
   return [ x.toString(10), y.toString(10) ];
